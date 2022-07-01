384727d7a7c5551ffb0477ad73ef05e7c83b6132
Good <artcraft.cz@gmail.com>
2022-07-01 18:52:15 +0200

    removed redundant code

--- frontend/src/Components/Karma.tsx
+++ frontend/src/Components/Karma.tsx
@@ -7,35 +7,28 @@ export function Karma() {
     const [profileVotingResult, setU] = useState(0); // sum of votes in profile
     const [punishment, setPunishment] = useState(0); // penalty set by moderator
 
-    const [contentRating, setContentRating] = useState(0); // content quality ratio
-    const [userRating, setUserRating] = useState(0); //user reputation ratio
-    const [rawKarma, setRawKarma] = useState(0); // karma without punishment
-    const [karma, setKarma] = useState(0);// karma with punishment
-
-    const positiveCommentsDivisor = 10; // positive comments are 10x cheaper than posts
-    const negativeCommentsDivisor = 2; // negative comments are only 2x cheaper than posts
-    //TODO extract the rest of coefficients to constants and comment them
-
     useEffect(() => {
-        setU( clamp(profileVotingResult, -profileVotesCount, profileVotesCount));
+         setU( clamp(profileVotingResult, -profileVotesCount, profileVotesCount));
     },[profileVotesCount]);
 
-    useEffect(() => {
-        const x = (allPostsValue + allCommentsValue/(allCommentsValue>=0?positiveCommentsDivisor:negativeCommentsDivisor))/5000;
-        setContentRating(x > 0 ? bipolarSigmoid(x/3) : Math.max(-1, -x*x*7) );
-    },[allPostsValue, allCommentsValue]);
+    //TODO extract the rest of coefficients to constants and comment them
 
-    useEffect(() => {
-        const x = profileVotingResult / profileVotesCount;
-        const ratio =  fit01(profileVotesCount, 10, 200, 0 ,1);
-        setUserRating( profileVotingResult >= 0 ? 1 : Math.max(0, 1 - lerp(Math.pow(x/100, 2), Math.pow(x*2, 2), ratio)));
-    },[profileVotingResult, profileVotesCount]);
+    //content quality ratio
+    const positiveCommentsDivisor = 10; // positive comments are 10x cheaper than posts
+    const negativeCommentsDivisor = 2; // negative comments are only 2x cheaper than posts
+    const contentVal = (allPostsValue + allCommentsValue/(allCommentsValue>=0?positiveCommentsDivisor:negativeCommentsDivisor))/5000;
+    const contentRating = (contentVal > 0 ? bipolarSigmoid(contentVal/3) : Math.max(-1, -Math.pow(contentVal,2)*7) );
 
-    useEffect(() => {
-        const k = Math.ceil(((contentRating+1)*userRating-1) *1000);
-        setRawKarma(k);
-        setKarma( Math.max(-1000, k - punishment) );
-    },[contentRating, userRating, punishment]);
+    //user reputation ratio
+    const ratio = profileVotingResult / profileVotesCount;
+    const s = fit01(profileVotesCount, 10, 200, 0 ,1);
+    const userRating = ( profileVotingResult >= 0 ? 1 : Math.max(0, 1 - lerp(Math.pow(ratio/100, 2), Math.pow(ratio*2, 2), s)));
+
+    // karma without punishment
+    const rawKarma = Math.ceil(((contentRating+1)*userRating-1) * 1000);
+
+    // karma with punishment
+    const karma = Math.max(-1000, rawKarma - punishment);
 
     return (
         <div id='karmaCalculator'>
@@ -64,7 +57,6 @@ export function Karma() {
                     <p> Рейтинг юзера: {userRating}</p>
                     <p> Сырая Карма: {rawKarma}</p>
                     <p> Карма: {karma}</p>
-
                 </div>
         </div>
     );
