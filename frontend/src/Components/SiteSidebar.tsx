168e5f5d7354efd685acb6d960ddcbae2f53ffad
Good <artcraft.cz@gmail.com>
2022-08-14 16:00:29 +0200

    fix: better menu UX on mobile

--- frontend/src/Components/SiteSidebar.tsx
+++ frontend/src/Components/SiteSidebar.tsx
@@ -5,16 +5,24 @@ import {useAPI, useAppState} from '../AppState/AppState';
 import {toast} from 'react-toastify';
 import {observer} from 'mobx-react-lite';
 
-export const SiteSidebar = observer(() => {
+type SidebarProps = {
+    onMenuToggle: () => void;
+};
+
+export const SiteSidebar = observer((props: SidebarProps) => {
     const [subsDisabled, setSubsDisabled] = useState(false);
     const api = useAPI();
     const {site, siteInfo, subscriptions} = useAppState();
+
+    const menuToggle = () => {
+        if(window.innerWidth <= 1179 ) props.onMenuToggle();
+    };
 
     const handleSubscribe = (subscribe: boolean) => {
         if (!siteInfo) {
             return;
         }
-        setSubsDisabled(true);
+        setSubsDisabled(false);
         api.site.subscribe(siteInfo.site, subscribe, false)
             .then(() => {
                 setSubsDisabled(false);
@@ -25,24 +33,27 @@ export const SiteSidebar = observer(() => {
             });
     };
 
-    return (<div className={styles.container}>
-        <div className='fixed'>
-            <Link className='site-name' to={site !== 'main' ? `/s/${site}` : '/'}> {siteInfo?.name || '...'}</Link>
-            {site !== 'main' &&
-            <div className='subscribe'>
-                {!siteInfo || siteInfo.subscribe?.main ?
-                    <button className='subscribed' disabled={!siteInfo || subsDisabled} onClick={() => handleSubscribe(false)}>Отписаться</button>
-                    :
-                    <button className='not-subscribed' disabled={!siteInfo || subsDisabled} onClick={() => handleSubscribe(true)}>Подписаться</button>
-                }
-            </div>}
-            {siteInfo?.siteInfo && <div className='site-info'>{siteInfo.siteInfo}</div>}
-            <div className='subsites'>
-                { subscriptions.map(site => {
-                    return <div key={site.site}><Link to={site.site === 'main' ? '/' : `/s/${site.site}`}>{site.name}</Link></div>;
-                }) }
+    return (<>
+            <div className={styles.fade} onClick={menuToggle}></div>
+            <div className={styles.container}>
+                <div className='fixed'>
+                    <Link className='site-name' to={site !== 'main' ? `/s/${site}` : '/'}> {siteInfo?.name || '...'}</Link>
+                    {site !== 'main' &&
+                    <div className='subscribe'>
+                        {!siteInfo || siteInfo.subscribe?.main ?
+                            <button className='subscribed' disabled={!siteInfo || subsDisabled} onClick={() => handleSubscribe(false)}>Отписаться</button>
+                            :
+                            <button className='not-subscribed' disabled={!siteInfo || subsDisabled} onClick={() => handleSubscribe(true)}>Подписаться</button>
+                        }
+                    </div>}
+                    {siteInfo?.siteInfo && <div className='site-info'>{siteInfo.siteInfo}</div>}
+                    <div className='subsites'>
+                        { subscriptions.map(site => {
+                            return <div key={site.site}><Link onClick={menuToggle} to={site.site === 'main' ? '/' : `/s/${site.site}`}>{site.name}</Link></div>;
+                        }) }
+                    </div>
+                    <Link onClick={menuToggle} className='all-subsites' to='/sites'>Все подсайты</Link>
+                </div>
             </div>
-            <Link className='all-subsites' to='/sites'>Все подсайты</Link>
-        </div>
-    </div>);
+        </>);
 });
