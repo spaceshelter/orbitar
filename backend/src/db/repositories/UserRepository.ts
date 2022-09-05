58e6a5c45ec22689927778687e39db8a1807b3f3
pazoozoo42 <takumo+pazoozoo@gmail.com>
2022-09-05 18:17:56 -0500

    linter

--- backend/src/db/repositories/UserRepository.ts
+++ backend/src/db/repositories/UserRepository.ts
@@ -32,7 +32,7 @@ export default class UserRepository {
     }
 
     async generateAndSavePasswordResetForUser(userId: number): Promise<string | undefined> {
-        const hash = crypto.createHash('md5').update(Math.random().toString() + userId).digest("hex");
+        const hash = crypto.createHash('md5').update(Math.random().toString() + userId).digest('hex');
         await this.db.insert('user_password_reset', {
             user_id: userId.toString(),
             code: hash
