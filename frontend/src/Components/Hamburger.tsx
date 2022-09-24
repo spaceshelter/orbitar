559ba2f6ea74c70e3de95fd1a4e42e23e7e78a0d
Hawkhub <fmwins1234@gmail.com>
2022-09-25 01:23:46 +0300

    Анимация выезжания/уезжания сайдбара и новый гамбургер (#144)
    * Change sidebar behavior: sliding sideways instead of unmounting
    * Nicer hamburger menu icon with changing to "closed" animation
    
    Co-authored-by: didim <Didim>
    Co-authored-by: Ivan Zaitsev <aiveeen@gmail.com>

---
+++ frontend/src/Components/Hamburger.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from './Hamburger.module.scss';
+import classNames from 'classnames';
+
+type HamburgerProps = {
+    open: boolean;
+};
+
+export const Hamburger = (props: HamburgerProps) => {
+    return (
+      <svg
+          className={classNames(styles.hamburger, {
+              [styles.open]: props.open,
+          })}
+          width="24"
+          height="24"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+      >
+          <rect x="3" y="5.99998" width="18" height="1.4"/>
+          <rect x="3" y="10.99998" width="18" height="1.4"/>
+          <rect x="3" y="15.99998" width="18" height="1.4"/>
+      </svg>
+    );
+};
