5f713a2e206b752128344a0cf523fa75412cdbdb
Ivan Zaitsev <aiveeen@gmail.com>
2022-09-25 15:30:21 -0700

    Revert "Hotfix v1.1.1 staging workflow (#126)"
    This reverts commit 95b186e8a932e157a934b2cffafe52f87cf69ab9.

--- backend/src/api/UserController.ts
+++ backend/src/api/UserController.ts
@@ -17,9 +17,6 @@ import {UserProfileEntity} from './types/entities/UserEntity';
 import VoteManager from '../managers/VoteManager';
 import {UserRatingBySubsite} from '../managers/types/UserInfo';
 
-// constant variables
-import { ERROR_CODES } from '../api/utils/error-codes';
-
 export default class UserController {
     public readonly router = Router();
     private readonly userManager: UserManager;
@@ -64,7 +61,7 @@ export default class UserController {
             const profileInfo = await this.userManager.getByUsernameWithVote(username, userId);
 
             if (!profileInfo) {
-                return response.error(ERROR_CODES.NOT_FOUND, 'User not found', 404);
+                return response.error('not-found', 'User not found', 404);
             }
 
             const invites = await this.userManager.getInvites(profileInfo.id);
@@ -101,7 +98,7 @@ export default class UserController {
             const profile = await this.userManager.getByUsername(username);
 
             if (!profile) {
-                return response.error(ERROR_CODES.NOT_FOUND, 'User not found', 404);
+                return response.error('not-found', 'User not found', 404);
             }
 
             const total = await this.postManager.getPostsByUserTotal(profile.id);
@@ -132,7 +129,7 @@ export default class UserController {
             const profile = await this.userManager.getByUsername(username);
 
             if (!profile) {
-                return response.error(ERROR_CODES.NOT_FOUND, 'User not found', 404);
+                return response.error('not-found', 'User not found', 404);
             }
 
             const total = await this.postManager.getUserCommentsTotal(profile.id);
@@ -162,7 +159,7 @@ export default class UserController {
             const profile = await this.userManager.getByUsername(username);
 
             if (!profile) {
-                return response.error(ERROR_CODES.NOT_FOUND, 'User not found', 404);
+                return response.error('not-found', 'User not found', 404);
             }
 
             const restrictions = await this.userManager.getUserRestrictions(profile.id);
@@ -193,7 +190,7 @@ export default class UserController {
             const profile = await this.userManager.getByUsername(username);
 
             if (!profile) {
-                return response.error(ERROR_CODES.NOT_FOUND, 'User not found', 404);
+                return response.error('not-found', 'User not found', 404);
             }
 
             const restrictions = await this.userManager.getUserRestrictions(profile.id);
