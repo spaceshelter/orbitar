209f0217398eada5e76ee24f2d20d37132559378
iamwavecut <wavecutdnb@gmail.com>
2022-10-10 15:05:59 +0200

    add: cleanup debug statements on prod build

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = function override(config, env) {
     config.output.publicPath = '//' + process.env.REACT_APP_ROOT_DOMAIN + '/';
@@ -15,6 +16,29 @@ module.exports = function override(config, env) {
 
     if (env === 'development') {
         config.plugins.push(new MiniCssExtractPlugin());
+    }
+
+    if (env === 'production') {
+        config.plugins.push(new UglifyJsPlugin({
+            uglifyOptions: {
+                comments: false,
+                global_defs: {
+                    '@alert': 'console.log',
+                },
+                compress: {
+                    drop_debugger: true,
+                    warnings: false,
+                    drop_console: true,
+                    pure_funcs: [
+                        'console.log',
+                        'console.info',
+                        'console.debug',
+                        'console.warn',
+                        'console.dir',
+                    ]
+                }
+            }
+        }));
     }
 
     config.module.rules[1].oneOf = [
