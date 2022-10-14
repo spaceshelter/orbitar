c4bb6e1189aea5d31fcd2ce903ea9fa19253a43c
4vanger <github@4vanger.com>
2022-10-13 18:39:26 -0700

    remove example. use 4 spaces indent

--- frontend/src/Pages/PostPage.tsx
+++ frontend/src/Pages/PostPage.tsx
@@ -1,182 +1,134 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useLocation, useParams, useSearchParams } from 'react-router-dom';
-
-import { usePost } from '../API/use/usePost';
-import { useAppState } from '../AppState/AppState';
-import CommentComponent from '../Components/CommentComponent';
-import { CreateCommentComponentRestricted } from '../Components/CreateCommentComponent';
-import PostComponent from '../Components/PostComponent';
-import { CommentInfo, PostInfo, PostLinkInfo } from '../Types/PostInfo';
+import React, {useEffect, useState} from 'react';
 import styles from './PostPage.module.css';
+import {Link, useLocation, useParams, useSearchParams} from 'react-router-dom';
+import {CommentInfo, PostInfo, PostLinkInfo} from '../Types/PostInfo';
+import PostComponent from '../Components/PostComponent';
+import CommentComponent from '../Components/CommentComponent';
+import {CreateCommentComponentRestricted} from '../Components/CreateCommentComponent';
+import {usePost} from '../API/use/usePost';
+import {useAppState} from '../AppState/AppState';
 
 export default function PostPage() {
-  const params = useParams<{ postId: string }>();
-  const [search] = useSearchParams();
-  const postId = params.postId ? parseInt(params.postId, 10) : 0;
-  const location = useLocation();
-  const [scrolledToComment, setScrolledToComment] = useState<{
-    postId: number;
-    commentId: number;
-  }>();
-  const { site } = useAppState();
+    const params = useParams<{postId: string}>();
+    const [search] = useSearchParams();
+    const postId = params.postId ? parseInt(params.postId, 10) : 0;
+    const location = useLocation();
+    const [scrolledToComment, setScrolledToComment] = useState<{postId: number, commentId: number}>();
+    const {site} = useAppState();
 
-  const unreadOnly = search.get('new') !== null;
-  const { post, comments, postComment, editComment, editPost, error, reload, updatePost } = usePost(
-    site,
-    postId,
-    unreadOnly,
-  );
+    const unreadOnly = search.get('new') !== null;
+    const {post, comments, postComment, editComment, editPost, error, reload, updatePost} = usePost(site, postId, unreadOnly);
 
-  useEffect(() => {
-    let docTitle = `Пост #${postId}`;
-    if (post) {
-      if (post.title) {
-        docTitle = post.title;
-      }
-      docTitle += ' / ' + post.author.username;
-    }
-    document.title = docTitle;
-  }, [post, postId]);
+    useEffect(() => {
+        let docTitle = `Пост #${postId}`;
+        if (post) {
+            if (post.title) {
+                docTitle = post.title;
+            }
+            docTitle += ' / ' + post.author.username;
+        }
+        document.title = docTitle;
 
-  const handleCommentEdit = async (text: string, comment: CommentInfo) => {
-    return await editComment(text, comment.id);
-  };
+    }, [post, postId]);
 
-  const handleAnswer = async (text: string, post?: PostLinkInfo, comment?: CommentInfo) => {
-    if (!post) {
-      return;
-    }
+    const handleCommentEdit = async (text: string, comment: CommentInfo) => {
+        return await editComment(text, comment.id);
+    };
 
-    const newComment = await postComment(text, comment?.id);
-    setTimeout(() => {
-      const el = document.querySelector(`div[data-comment-id="${newComment.id}"]`);
-      el?.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
-    }, 100);
-    return comment;
-  };
-
-  useEffect(() => {
-    if (!comments) {
-      return;
-    }
-
-    if (location.hash) {
-      const commentId = parseInt(location.hash.substring(1));
-
-      if (
-        scrolledToComment &&
-        scrolledToComment.postId === postId &&
-        scrolledToComment.commentId === commentId
-      ) {
-        return;
-      }
-
-      const el = document.querySelector(`[data-comment-id="${commentId}"] .commentBody`);
-      if (el) {
-        setTimeout(() => {
-          el.scrollIntoView({ behavior: 'smooth', block: 'center' });
-          el.classList.add('highlight');
-
-          setTimeout(() => {
-            el.classList.remove('highlight');
-            setScrolledToComment({ postId, commentId });
-          }, 5000);
-        }, 100);
-      }
-    } else if (unreadOnly) {
-      // find new comment
-      const el = document.querySelector(`.isNew`);
-      if (el) {
-        const commentId = parseInt(el.getAttribute('data-comment-id') || '');
-        if (!commentId) {
-          return;
+    const handleAnswer = async (text: string, post?: PostLinkInfo, comment?: CommentInfo) => {
+        if (!post) {
+            return;
         }
 
-        if (
-          scrolledToComment &&
-          scrolledToComment.postId === postId &&
-          scrolledToComment.commentId === commentId
-        ) {
-          return;
+        const newComment = await postComment(text, comment?.id);
+        setTimeout(() => {
+            const el = document.querySelector(`div[data-comment-id="${newComment.id}"]`);
+            el?.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+        }, 100);
+        return comment;
+    };
+
+    useEffect(() => {
+        if (!comments) {
+            return;
         }
 
-        setTimeout(() => {
-          el.scrollIntoView({ behavior: 'smooth', block: 'center' });
+        if (location.hash) {
+            const commentId = parseInt(location.hash.substring(1));
 
-          setTimeout(() => {
-            setScrolledToComment({ postId, commentId });
-          }, 5000);
-        }, 100);
-      }
-    }
-  }, [location.hash, comments, scrolledToComment, unreadOnly]);
+            if (scrolledToComment && scrolledToComment.postId === postId && scrolledToComment.commentId === commentId) {
+                return;
+            }
 
-  const handlePostEdit = async (
-    post: PostInfo,
-    text: string,
-    title?: string,
-  ): Promise<PostInfo | undefined> => {
-    return await editPost(title || '', text);
-  };
+            const el = document.querySelector(`[data-comment-id="${commentId}"] .commentBody`);
+            if (el) {
+                setTimeout(() => {
+                    el.scrollIntoView({ behavior: 'smooth', block: 'center' });
+                    el.classList.add('highlight');
 
-  const baseRoute = site === 'main' ? '/' : `/s/${site}/`;
+                    setTimeout(() => {
+                        el.classList.remove('highlight');
+                        setScrolledToComment({postId, commentId});
+                    }, 5000);
+                }, 100);
+            }
+        }
+        else if (unreadOnly) {
+            // find new comment
+            const el = document.querySelector(`.isNew`);
+            if (el) {
+                const commentId = parseInt(el.getAttribute('data-comment-id') || '');
+                if (!commentId) {
+                    return;
+                }
 
-  return (
-    <div className={styles.container}>
-      <div className={styles.feed}>
-        {post ? (
-          <div>
-            <PostComponent
-              key={post.id}
-              post={post}
-              onChange={(_, partial) => updatePost(partial)}
-              onEdit={handlePostEdit}
-            />
-            <div className={styles.postButtons}>
-              <Link to={`${baseRoute}p${post.id}`} className={unreadOnly ? '' : 'bold'}>
-                все комментарии
-              </Link>{' '}
-              •{' '}
-              <Link to={`${baseRoute}p${post.id}?new`} className={unreadOnly ? 'bold' : ''}>
-                только новые
-              </Link>
+                if (scrolledToComment && scrolledToComment.postId === postId && scrolledToComment.commentId === commentId) {
+                    return;
+                }
+
+                setTimeout(() => {
+                    el.scrollIntoView({ behavior: 'smooth', block: 'center' });
+
+                    setTimeout(() => {
+                        setScrolledToComment({postId, commentId});
+                    }, 5000);
+                }, 100);
+            }
+
+        }
+    }, [location.hash, comments, scrolledToComment, unreadOnly]);
+
+    const handlePostEdit = async (post: PostInfo, text: string, title?: string): Promise<PostInfo | undefined> => {
+        return await editPost(title || '', text);
+    };
+
+    const baseRoute = site === 'main' ? '/' : `/s/${site}/`;
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.feed}>
+                {post ? <div>
+                        <PostComponent key={post.id} post={post} onChange={(_, partial) => updatePost(partial)} onEdit={handlePostEdit} />
+                        <div className={styles.postButtons}><Link to={`${baseRoute}p${post.id}`} className={unreadOnly ? '' : 'bold'}>все комментарии</Link> • <Link to={`${baseRoute}p${post.id}?new`} className={unreadOnly ? 'bold' : ''}>только новые</Link></div>
+                        <div className={styles.comments + (unreadOnly ? ' unreadOnly' : '')}>
+                            {comments ?
+                                comments.map(comment => <CommentComponent maxTreeDepth={12} key={comment.id} comment={comment} onAnswer={handleAnswer} unreadOnly={unreadOnly} onEdit={handleCommentEdit} />)
+                                :
+                                (
+                                    error ? <div className={styles.error}>{error}<div><button onClick={() => reload(unreadOnly)}>Повторить</button></div></div>
+                                        : <div className={styles.loading}>Загрузка...</div>
+                                )
+                            }
+                        </div>
+                        <CreateCommentComponentRestricted open={true} post={post} onAnswer={handleAnswer} />
+                    </div>
+                    :
+                    (
+                        error ? <div className={styles.error}>{error}<div><button onClick={() => reload(unreadOnly)}>Повторить</button></div></div>
+                        : <div className={styles.loading}>Загрузка...</div>
+                    )
+                }
             </div>
-            <div className={styles.comments + (unreadOnly ? ' unreadOnly' : '')}>
-              {comments ? (
-                comments.map((comment) => (
-                  <CommentComponent
-                    maxTreeDepth={12}
-                    key={comment.id}
-                    comment={comment}
-                    onAnswer={handleAnswer}
-                    unreadOnly={unreadOnly}
-                    onEdit={handleCommentEdit}
-                  />
-                ))
-              ) : error ? (
-                <div className={styles.error}>
-                  {error}
-                  <div>
-                    <button onClick={() => reload(unreadOnly)}>Повторить</button>
-                  </div>
-                </div>
-              ) : (
-                <div className={styles.loading}>Загрузка...</div>
-              )}
-            </div>
-            <CreateCommentComponentRestricted open={true} post={post} onAnswer={handleAnswer} />
-          </div>
-        ) : error ? (
-          <div className={styles.error}>
-            {error}
-            <div>
-              <button onClick={() => reload(unreadOnly)}>Повторить</button>
-            </div>
-          </div>
-        ) : (
-          <div className={styles.loading}>Загрузка...</div>
-        )}
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
