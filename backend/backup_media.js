53bf48043c578f59d968f61a6a1196de579782d1
pazoozoo42 <takumo+pazoozoo@gmail.com>
2022-10-24 19:22:01 -0500

    Make http requests synchronous, fetch mysql entries in batches

--- backend/backup_media.js
+++ backend/backup_media.js
@@ -8,6 +8,8 @@ const sizeOf = require('image-size');
 
 const MEDIA_TYPE_IMG = 'img';
 const MEDIA_TYPE_VIDEO = 'video';
+const SLEEP_BETWEEN_BATCHES_MS = 1000;
+const BATCH_SIZE = 1000;
 
 const config = {
     host: process.env.MYSQL_HOST || "localhost",
@@ -15,8 +17,8 @@ const config = {
     user: "root",
     password: process.env.MYSQL_ROOT_PASSWORD || "orbitar",
     database: process.env.MYSQL_DATABASE || "orbitar_db",
-    dir: './media-backup',
-    dbFile: './media-backup/index.csv'
+    dir: './backup-media',
+    dbFile: './backup-media/index.csv'
 };
 
 const sleep = function(ms) {
@@ -25,30 +27,47 @@ const sleep = function(ms) {
   });
 }
 
-const download = function(url, dest, cb) {
-  if (url.match(/^\/\//)) {
-    url = 'https:' + url;
-  }
-  try {
-    const file = fs.createWriteStream(dest);
-    let loader = http;
-    if (url.includes('https://')) {
-      loader = https;
+const download = async function(url, dest) {
+  return new Promise((resolve) => {
+    if (url.match(/^\/\//)) {
+      url = 'https:' + url;
     }
-    loader.get(url, function(response) {
-      response.pipe(file);
-      file.on('finish', function() {
-        file.close(cb);
+    try {
+      const file = fs.createWriteStream(dest);
+      let loader = http;
+      if (url.includes('https://')) {
+        loader = https;
+      }
+      loader.get(url, function(response) {
+        response.pipe(file);
+        file.on('finish', function() {
+          file.close();
+          resolve(true);
+        });
+      }).on('error', function(err) {
+        console.trace(err);
+        fs.unlinkSync(dest);
+        resolve(false);
       });
-    }).on('error', function(err) {
-      console.trace(err);
-      fs.unlinkSync(dest);
-      if (cb) cb(err.message);
-    });
-  } catch (e) {
-    console.trace(e);
-  }
+    } catch (e) {
+      console.trace(e);
+      resolve(false);
+    }
+  })
 };
+
+const saveEntry = function(mediaType, outputPath, outputFilename, mediaSrc, itemType, itemId) {
+  let dimensions = {width: 0, height: 0};
+  if (mediaType === MEDIA_TYPE_IMG) {
+    try {
+      dimensions = sizeOf(outputPath);
+    } catch (e) {}
+  }
+  let line = [
+    itemType, itemId, mediaType, mediaSrc, outputFilename, dimensions.width, dimensions.height
+  ];
+  fs.appendFileSync(config.dbFile, line.join(',') + "\n");
+}
 
 try { fs.mkdirSync(config.dir); } catch (e) {}
 if (!fs.existsSync(config.dbFile)) {
@@ -65,42 +84,29 @@ const connection = mysql.createConnection({
   database: config.database
 });
 
-const entries = [];
+const processBatch = async (offset) => {
+  const result = await connection.promise().query(
+    'select * from `content_source` where `source` like "%<img%" or `source` like "%<video%" limit ' + offset + ',' + BATCH_SIZE
+  );
 
-connection.query(
-  'SELECT html, "comment" as type, comment_id as "item_id" FROM `comments` WHERE `html` like "%<img%" UNION ' +
-  'SELECT html, "comment" as type, comment_id as "item_id" FROM `comments` WHERE `html` like "%<video%" UNION ' +
-  'SELECT html, "post" as type, post_id as "item_id" FROM `posts` WHERE `html` like "%<img%" UNION ' +
-  'SELECT html, "post" as type, post_id as "item_id" FROM `posts` WHERE `html` like "%<video%"',
-  async function (err, results, fields) {
-    if (err) {
-      console.trace(err);
-      process.exit(1);
-    }
-    console.log('Total items with media content: ', results.length);
-    for (let i = 0; i < results.length; i++) {
-      entries.push(results[i]);
-      if (i % 1000 === 0) {
-        console.log(`Fetched ${entries.length} entries`);
-      }
-    }
-    await startDownload();
+  if (!result[0] || !result[0].length) {
+    console.log('Done');
     process.exit(0);
-});
+  }
 
-const startDownload = async function() {
-  console.log('Starting processing');
-  for (let i = 0; i < entries.length; i++) {
-    let entry = entries[i];
-    let itemType = entry.type;
-    let itemId = entry.item_id;
-    let entryHtml = entry.html;
-
-    const mediaItems = entryHtml.match(/<(img|source).+src=['"]([^'"]+)['"]/g);
+  for (let i = 0; i < result[0].length; i++) {
+    let entry = result[0][i];
+    let itemType = entry.ref_type;
+    let itemId = entry.ref_id;
+    let entrySource = entry.source;
+    const mediaItems = entrySource.match(/<(img|source).+src=['"]([^'"]+)['"]/g);
+    if (!mediaItems) {
+      continue;
+    }
     for (let ii = 0; ii < mediaItems.length; ii++) {
       const check = mediaItems[ii].match(/<(img|source).+src=['"]([^'"]+)['"]/);
-      let mediaType = check[1] === MEDIA_TYPE_IMG ? MEDIA_TYPE_IMG : MEDIA_TYPE_VIDEO;
       let mediaSrc = check[2];
+      let mediaType = check[1] === MEDIA_TYPE_IMG ? MEDIA_TYPE_IMG : MEDIA_TYPE_VIDEO;
       let mediaExtensionMatch = mediaSrc.match(/\.[a-z]{2,}(?:\?.+)?$/);
       let mediaExtension;
       if (!mediaExtensionMatch) {
@@ -112,40 +118,38 @@ const startDownload = async function() {
       } else {
         mediaExtension = mediaExtensionMatch[0].replace(/\?.+?$/, '');
       }
-
       let outputFilename = crypto.createHash('sha256').update(mediaSrc).digest('hex') + mediaExtension;
       let outputPath = config.dir + '/' + outputFilename;
-      let dimensions = {width: 0, height: 0};
-
-      const saveEntry = function() {
-        if (mediaType === MEDIA_TYPE_IMG) {
-          try {
-            dimensions = sizeOf(outputPath);
-          } catch (e) {
-            dimensions = {width: 0, height: 0};
-          }
-        }
-        let line = [
-          itemType, itemId, mediaType, mediaSrc, outputFilename, dimensions.width, dimensions.height
-        ];
-        fs.appendFileSync(config.dbFile, line.join(',') + "\n");
-      }
-
       if (fs.existsSync(outputPath)) {
         console.log(outputPath, 'is already there');
-        saveEntry();
+        saveEntry(mediaType, outputPath, outputFilename, mediaSrc, itemType, itemId);
         continue;
       }
-      download(mediaSrc, outputPath, async function() {
-        saveEntry();
-        console.log('Downloaded:', itemType, itemId, mediaType, mediaSrc, config.dir + '/' + outputFilename, dimensions.width, 'x', dimensions.height);
-      });
-      await sleep(500);
-      if (i % 1000 === 0) {
-        console.log(`Processed ${i} entries`);
+      try {
+        console.log(entrySource);
+        console.log(`Downloading ${mediaSrc} to ${outputPath}`);
+        if (await download(mediaSrc, outputPath)) {
+          saveEntry(mediaType, outputPath, outputFilename, mediaSrc, itemType, itemId);
+          console.log('Downloaded: ', mediaSrc);
+        }
+      } catch (e) {
+        console.error('Failed to download: ', mediaSrc);
       }
     }
   }
-};
+  await sleep(SLEEP_BETWEEN_BATCHES_MS);
+  await processBatch(offset + BATCH_SIZE);
+}
 
-
+(async () => {
+  const result = await connection.promise().query(
+    'select count(*) as total from `content_source` where `source` like "%<img%" or `source` like "%video%"'
+  );
+  console.log('Content sources with media: ', result[0][0].total);
+  console.log('Start processing');
+  await processBatch(0);
+  process.exit(0);
+})().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
---
+++ backup-media.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+MYSQL_CONTAINER=$(docker ps --format "{{.Names}}" | grep mysql | grep orbitar)
+docker run --rm -it --name=orbitar-backup-media -v ${PWD}/backend:/app --network container:$MYSQL_CONTAINER --entrypoint='/bin/bash' node:17 -c "cd /app/ && npm install && node backup_media.js"
