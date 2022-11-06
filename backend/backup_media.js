726fca3aa5e5e46470d0ad1f4c2c0938d75316c1
pazoozoo42 <takumo+pazoozoo@gmail.com>
2022-11-06 11:01:18 -0600

    final version

--- backend/backup_media.js
+++ backend/backup_media.js
@@ -4,15 +4,12 @@ const fs = require('fs');
 const crypto = require('crypto');
 const fetch = require('node-fetch');
 const sizeOf = require('image-size');
-const setTimeoutPromise = require('node:timers/promises').setTimeout;
 
 const MEDIA_TYPE_IMG = 'img';
 const MEDIA_TYPE_VIDEO = 'video';
 const SLEEP_BETWEEN_BATCHES_MS = 1000;
-const SLEEP_BETWEEN_MEDIA_ITEMS_MS = 100;
 const BATCH_SIZE = 1000;
-const MEDIA_DOWNLOAD_REQUEST_TIMEOUT_MS = 60000;
-const START_DOWNLOADING_FROM_DATE = '2022-01-01';
+const MEDIA_DOWNLOAD_REQUEST_TIMEOUT_MS = 120000;
 
 const config = {
   host: process.env.MYSQL_HOST || 'localhost',
@@ -21,41 +18,31 @@ const config = {
   password: process.env.MYSQL_ROOT_PASSWORD || 'orbitar',
   database: process.env.MYSQL_DATABASE || 'orbitar_db',
   dir: './backup-media',
-  dbFile: './backup-media/index.csv'
+  dbFile: './backup-media/index.csv',
+  lastProcessedIdFile: './backup-media/id.txt'
 };
 
 const sleep = ms => new Promise(r => setTimeout(r, ms));
 
-const download = async (url, dest, controller) => {
+const download = async (url, dest, signal) => {
     if (!url.match(/^(\/\/|http:|https:)/)) {
-      console.log(`Invalid URL: `, url);
+      console.log(`Invalid URL: ${url}`);
       return false;
     }
     if (url.match(/^\/\//)) {
       url = 'https:' + url;
     }
     try {
-      const response = await fetch(url, { signal: controller.signal });
+      const response = await fetch(url, { signal });
       const responseBinary = await response.buffer();
       let writeResult = await fs.promises.writeFile(dest, responseBinary);
       await sleep(1000);
       console.log(`Wrote ${responseBinary.length} bytes to ${dest} for ${url}`);
-      return controller.abort();
+      return true;
     } catch (e) {
       console.log(e);
       return false;
     }
-};
-
-const downloadRequestTimeout = async (ms, url, controller) => {
-  try {
-    await setTimeoutPromise(ms, undefined, { signal: controller.signal });
-    controller.abort();
-    console.log(`Request to fetch ${url} is aborted due to slow response`);
-    return false;
-  } catch (error) {
-    return false;
-  }
 };
 
 const connection = mysql.createConnection({
@@ -68,11 +55,13 @@ const connection = mysql.createConnection({
 try {
   fs.mkdirSync(config.dir);
 } catch (e) {}
+
 if (!fs.existsSync(config.dbFile)) {
-  fs.openSync(config.dbFile, 'w+');
-  fs.appendFileSync(config.dbFile, 'item_type,item_id,media_type,media_src,media_id,img_width,img_height  ' + '\n');
-} else {
-  fs.openSync(config.dbFile, 'w+');
+  fs.writeFileSync(config.dbFile, 'type,content_source_id,media_type,media_src,media_id,img_width,img_height  ' + '\n');
+}
+
+if (!fs.existsSync(config.lastProcessedIdFile)) {
+  fs.writeFileSync(config.lastProcessedIdFile, '0');
 }
 
 const saveEntry = (mediaType, outputPath, outputFilename, mediaSrc, itemType, itemId) => {
@@ -87,22 +76,37 @@ const saveEntry = (mediaType, outputPath, outputFilename, mediaSrc, itemType, it
     itemType, itemId, mediaType, mediaSrc, outputFilename, dimensions.width, dimensions.height
   ];
   fs.appendFileSync(config.dbFile, line.join(',') + '\n');
+  fs.writeFileSync(config.lastProcessedIdFile, itemId.toString());
 };
 
-const processBatch = async (offset) => {
+const processBatch = async (startWithId) => {
   const result = await connection.promise().query(
-    'select * from `content_source` where (`source` like "%<img%" or `source` like "%<video%") and date(created_at) > "' + START_DOWNLOADING_FROM_DATE + '" limit ' + offset + ',' + BATCH_SIZE
+    `select 
+        cs.content_source_id,
+        coalesce(p.html, c.html) as html,
+        cs.ref_type,
+        cs.ref_id
+    from
+        content_source cs
+    left join comments c on c.content_source_id = cs.content_source_id
+    left join posts p on p.content_source_id = cs.content_source_id
+    where
+        (p.html like '%<img%' or c.html like '%<img%' or
+         p.html like '%<video%' or c.html like '%<video%') and cs.content_source_id > ${startWithId}
+    order by cs.content_source_id asc
+    limit ${BATCH_SIZE}`
   );
   if (!result[0] || !result[0].length) {
-    return false;
+    return [false];
   }
+  let itemId;
   for (const entry of result[0]) {
+    itemId = entry.content_source_id;
     let itemType = entry.ref_type;
-    let itemId = entry.ref_id;
-    let entrySource = entry.source;
-    const mediaItems = entrySource.match(/<(img|video|source).+src=['"]([^'"]+)['"]/g);
+    let entryHtml = entry.html;
+    const mediaItems = entryHtml.match(/<(img|video|source).+src=['"]([^'"]+)['"]/g);
     if (!mediaItems) {
-      console.log(`No media items detected: `, entrySource);
+      console.log(`No media items detected: `, entryHtml);
       continue;
     }
     console.log(`have ${mediaItems.length} media items`);
@@ -129,36 +133,40 @@ const processBatch = async (offset) => {
       } else {
         console.log(`Processing media item `, mediaSrc);
         try {
-          const requestController = new AbortController();
-          const requestTimeoutController = new AbortController();
-          await Promise.race([downloadRequestTimeout(MEDIA_DOWNLOAD_REQUEST_TIMEOUT_MS, mediaSrc, requestController), download(mediaSrc, outputPath, requestTimeoutController)]);
-          console.log(`Done processing media item ${mediaSrc}`);
-          saveEntry(mediaType, outputPath, outputFilename, mediaSrc, itemType, itemId);
-          await sleep(SLEEP_BETWEEN_MEDIA_ITEMS_MS);
+          const controller = new AbortController();
+          const succeeded = await Promise.race([
+            download(mediaSrc, outputPath, controller.signal),
+            sleep(MEDIA_DOWNLOAD_REQUEST_TIMEOUT_MS)
+          ]);
+          if (succeeded) {
+            console.log(`Done processing media item!`);
+            saveEntry(mediaType, outputPath, outputFilename, mediaSrc, itemType, itemId);
+          } else {
+            console.log(`Timeout processing media item!`);
+            controller.abort();
+          }
         } catch (e) {
           console.error(e);
         }
       }
     }
   }
-  return result[0].length;
+  return [result[0].length, itemId];
 };
 
 (async () => {
+  let lastProcessedId = parseInt(fs.readFileSync(config.lastProcessedIdFile).toString(), 10);
   try {
-    const result = await connection.promise().query(
-      'select count(*) as total from `content_source` where (`source` like "%<img%" or `source` like "%video%") and date(created_at) > "' + START_DOWNLOADING_FROM_DATE + '" '
-    );
-    console.log('Content sources with media:', result[0][0].total);
     console.log('Start processing');
-    let offset = 0;
     while (true) {
-      let processed = await processBatch(offset);
-      console.log(`Batch of ${processed} entries is done`);
-      if (processed < BATCH_SIZE) {
+      let [processedCount, lastBatchProcessedId] = await processBatch(lastProcessedId);
+      if (lastBatchProcessedId) {
+        console.log(`Batch of ${processedCount} is done, last processed item id: ${lastBatchProcessedId}`);
+      }
+      if (processedCount < BATCH_SIZE) {
         break;
       }
-      offset += processed;
+      lastProcessedId = lastBatchProcessedId;
       await sleep(SLEEP_BETWEEN_BATCHES_MS);
     }
   } catch (e) {
@@ -166,5 +174,6 @@ const processBatch = async (offset) => {
   } finally {
     console.log(`done`);
     connection.end();
+    process.exit();
   }
 })();
