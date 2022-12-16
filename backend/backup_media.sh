6375044debc2d66880ce0200a6f4c65acb98ed04
Aivean <aiveeen@gmail.com>
2022-12-15 18:16:14 -0800

    fail media backup step if  ` backup_media.js` fails

--- backend/backup_media.sh
+++ backend/backup_media.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -ex
+
 MYSQL_CONTAINER=$(docker ps --format "{{.Names}}" | grep mysql | grep orbitar)
 # get backup dir from the first argument
 BACKUP_DIR=$1
@@ -7,7 +9,6 @@ if [ -z "$BACKUP_DIR" ]; then
     exit 1
 fi
 
-# 30 minutes timeout
 docker run --rm -i --name=orbitar-backup-media -v "${PWD}":/app -v "$BACKUP_DIR":/orbitar_backup --network container:"$MYSQL_CONTAINER" \
   -e "MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD" \
   --entrypoint='/bin/bash' node:17 -c "cd /app/ && npm install && node backup_media.js /orbitar_backup"
