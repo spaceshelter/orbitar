f0c4159d47dc5c8e5530287c0a4f793b07880cdd
pazoozoo42 <57953386+pazoozoo42@users.noreply.github.com>
2022-12-23 16:28:49 -0600

    "Preview with another theme" button (#217)
    * Add "preview with another theme" button to content creation component
    
    * match height of textarea and preview
    
    * use state

--- frontend/src/Components/UserProfileSettings.tsx
+++ frontend/src/Components/UserProfileSettings.tsx
@@ -1,12 +1,10 @@
 import React, {useEffect} from 'react';
-import styles from '../Pages/UserPage.module.scss';
+import buttonStyles from '../Components/Buttons.module.scss';
 import {ReactComponent as LogoutIcon} from '../Assets/logout.svg';
 import {useAPI, useAppState} from '../AppState/AppState';
 import {useLocation, useNavigate, useParams} from 'react-router-dom';
 import {useUserProfile} from '../API/use/useUserProfile';
-import {ReactComponent as DarkIcon} from '../Assets/theme_dark.svg';
-import {ReactComponent as LightIcon} from '../Assets/theme_light.svg';
-import {useTheme} from '../Theme/ThemeProvider';
+import ThemeToggleComponent from './ThemeToggleComponent';
 
 export default function UserProfileSettings() {
   useEffect(() => {
@@ -20,7 +18,6 @@ export default function UserProfileSettings() {
   const params = useParams<{username?: string, page?: string}>();
   const username = params.username || userInfo?.username;
   const [state] = useUserProfile(username || '');
-  const {theme, setTheme} = useTheme();
 
   let isMyProfile: boolean | undefined = false;
   if (state.status === 'ready') {
@@ -34,23 +31,10 @@ export default function UserProfileSettings() {
     });
   };
 
-  const toggleTheme = (e: React.MouseEvent) => {
-    e.preventDefault();
-    if (theme === 'dark') {
-      setTheme('light');
-    }
-    else {
-      if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-        if (theme === 'light') { setTheme('debugTheme'); return;  }
-      }
-      setTheme('dark');
-    }
-  };
-
   return (
     <div>
-      <button className={styles.theme} onClick={toggleTheme}>{theme === 'dark' ? <LightIcon /> : <DarkIcon />} Сменить тему</button>
-      { isMyProfile && <button className={styles.logout} onClick={handleLogout}><LogoutIcon /> Выйти </button> }
+      { isMyProfile && <ThemeToggleComponent dynamic={true} buttonLabel='Сменить тему' /> }
+      { isMyProfile && <button className={buttonStyles.logoutButton} onClick={handleLogout}><LogoutIcon /> Выйти </button> }
     </div>
   );
 }
