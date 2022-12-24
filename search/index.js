608c51d9a20954e3dc6c238ce3d0dc362ddd2217
pazoozoo42 <takumo+pazoozoo@gmail.com>
2022-12-24 14:18:13 -0600

    Fix html injection possible in searched documents title

--- search/index.js
+++ search/index.js
@@ -67,7 +67,7 @@ async function main() {
         comment_post_id: entry.comment_post_id,
         gold: entry.gold,
         rating: entry.rating,
-        title: stripHtml(entry.title),
+        title: stripHtml(entry.title).result,
         source: stripHtml(entry.source).result
       });
     }
