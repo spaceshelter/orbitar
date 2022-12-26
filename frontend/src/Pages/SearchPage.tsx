5e4ab804dfab658281784caf1c9039f5a98d0bac
pazoozoo42 <57953386+pazoozoo42@users.noreply.github.com>
2022-12-26 11:34:35 -0600

    Search improvements (#224)
    * use phrase search
    
    * change search highlights styling
    
    * better query for multi word searches, highlight matches natively
    
    * fix z-index, more query fine-tuning
    
    * z-index not needed

--- frontend/src/Pages/SearchPage.tsx
+++ frontend/src/Pages/SearchPage.tsx
@@ -78,6 +78,7 @@ export default function SearchPage() {
             return;
         }
         setSearching(true);
+        setError('');
         setSearchParams({term});
         api.searchApi.search(term)
             .then((result) => {
