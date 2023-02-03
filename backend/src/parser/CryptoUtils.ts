095c72c000b23b794c049ea621e325e831cf945f
Ivan Zaitsev <aiveeen@gmail.com>
2023-02-02 16:17:38 -0800

    avoid exception on malformed input in aes decode (#267)

--- backend/src/parser/CryptoUtils.ts
+++ backend/src/parser/CryptoUtils.ts
@@ -8,12 +8,16 @@ export function aesEncryptToBase64(data, key) {
 }
 
 export function aesDecryptFromBase64(base64data, key) {
-    const decoded = CryptoJS.enc.Base64.parse(base64data);
-    const iv = CryptoJS.lib.WordArray.create(decoded.clone().words.slice(0, 4));
-    const ciphertext = CryptoJS.lib.WordArray.create(decoded.clone().words.slice(4));
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const decrypted = CryptoJS.AES.decrypt({ciphertext} as any, CryptoJS.enc.Utf8.parse(key), {
-        iv
-    });
-    return decrypted.toString(CryptoJS.enc.Utf8);
+    try {
+        const decoded = CryptoJS.enc.Base64.parse(base64data);
+        const iv = CryptoJS.lib.WordArray.create(decoded.clone().words.slice(0, 4));
+        const ciphertext = CryptoJS.lib.WordArray.create(decoded.clone().words.slice(4));
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const decrypted = CryptoJS.AES.decrypt({ciphertext} as any, CryptoJS.enc.Utf8.parse(key), {
+            iv
+        });
+        return decrypted.toString(CryptoJS.enc.Utf8);
+    } catch (e) {
+        return '';
+    }
 }
