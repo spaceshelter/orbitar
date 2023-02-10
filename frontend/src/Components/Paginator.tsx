b9fa9964577d9232f955ccdd6a07b6b03b1ae25a
pazoozoo42 <takumo+pazoozoo@gmail.com>
2023-02-10 17:27:17 -0600

    Simple pager

--- frontend/src/Components/Paginator.tsx
+++ frontend/src/Components/Paginator.tsx
@@ -1,6 +1,7 @@
 import {Link} from 'react-router-dom';
 import React from 'react';
-import styles from './Paginator.module.css';
+import styles from './Paginator.module.scss';
+import classNames from 'classnames';
 
 interface PaginatorProps {
     page: number;
@@ -10,24 +11,61 @@ interface PaginatorProps {
 }
 
 export default function Paginator(props: PaginatorProps) {
-    const pages = [];
-    for (let i = 1; i <= props.pages; i++) {
-        const classes = [styles.page];
-        if (i === props.page) {
-            classes.push(styles.current);
-        }
-        const params = new URLSearchParams(props.queryStringParams || {});
-        if (i > 1 || (params && Object.keys(params).length)) {
-            params.set('page', i.toString());
-        }
-        const search = params.toString();
+    const {page, base, queryStringParams} = props;
+    let {pages} = props;
 
-        pages.push(<Link key={i} className={classes.join(' ')} to={{pathname: props.base, search}}>{i}</Link>);
+    const range = 5;
+    const links = [];
+
+    const rangeMin = (range % 2 === 0) ? (range / 2) - 1 : (range - 1) / 2;
+    const rangeMax = (range % 2 === 0) ? rangeMin + 1 : rangeMin;
+    let pageMin = page - rangeMin;
+    let pageMax = page + rangeMax;
+
+    const params = new URLSearchParams(queryStringParams || {});
+    let search = params.toString();
+
+    pageMin = (pageMin < 1) ? 1 : pageMin;
+    pageMax = (pageMax < (pageMin + range - 1)) ? pageMin + range - 1 : pageMax;
+    if (pageMax > pages) {
+        pageMin = (pageMin > 1) ? pages - range + 1 : 1;
+        pageMax = pages;
     }
 
+    pageMin = (pageMin < 1) ? 1 : pageMin;
+
+    if (isNaN(pageMin))
+        pageMin = 0;
+    if (isNaN(pages))
+        pages = 0;
+    if (isNaN(pageMax))
+        pageMax = 0;
+
+    links.push(<Link key={-2} className={classNames(styles.page, styles.control, {[styles.disabled]: page === 1})} to={{pathname: base, search}}>⇤</Link>);
+
+    if (page - 1 !== 1) {
+        params.set('page', (page - 1).toString());
+    }
+    search = params.toString();
+    links.push(<Link key={-1} className={classNames(styles.page, styles.control, {[styles.disabled]: page === 1})} to={{pathname: base, search}}>←</Link>);
+
+    for (let i = pageMin; i <= pageMax; i++) {
+        params.set('page', i.toString());
+        search = params.toString();
+        links.push(<Link key={i} className={classNames(styles.page, {[styles.disabled]: i === page, [styles.current]: i === page})} to={{pathname: base, search}}>{i}</Link>);
+    }
+
+    if (page + 1 !== pages) {
+        params.set('page', (page + 1).toString());
+    }
+    search = params.toString();
+    links.push(<Link key={pages + 1} className={classNames(styles.page, styles.control, {[styles.disabled]: page === pages})} to={{pathname: base, search}}>→</Link>);
+
+    params.set('page', pages.toString());
+    search = params.toString();
+    links.push(<Link key={pages + 2} className={classNames(styles.page, styles.control, {[styles.disabled]: page === pages})} to={{pathname: base, search}}>⇥</Link>);
+
     return (
-        <div className={styles.paginator}>
-            {pages}
-        </div>
+      <div className={styles.paginator}>{links}</div>
     );
 }
