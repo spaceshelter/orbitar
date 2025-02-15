1dc26e332d30b330e355d5fea6c9f8ce11faa695
Aivean <aiveeen@gmail.com>
2025-02-14 17:45:54 -0800

    disallow fragments in redirect urls

--- backend/src/api/ApiMiddleware.ts
+++ backend/src/api/ApiMiddleware.ts
@@ -142,6 +142,7 @@ export const urisListValidator = Joi.string().custom((value, helpers) => {
         if (!isURL(url, {
             ...(process.env.NODE_ENV === 'development' && { host_whitelist: ['localhost'] }),
             require_protocol: true,
+            allow_fragments: false, /*RFC 6749 Section 3.1.2: The redirection endpoint URI MUST NOT include a fragment component.*/
             protocols: ['https', ...(process.env.NODE_ENV === 'development' ? ['http'] : [])]
         })) {
             return helpers.error('any.invalid');
