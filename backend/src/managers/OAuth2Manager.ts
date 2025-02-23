48eb6aeefc95b86793c0023ef783e32ec88de51a
Ivan Zaitsev <aiveeen@gmail.com>
2025-02-23 13:20:33 -0800

    remove excessive user info from oauth endpoint results (#416)

--- backend/src/managers/OAuth2Manager.ts
+++ backend/src/managers/OAuth2Manager.ts
@@ -163,7 +163,7 @@ export default class OAuth2Manager {
       grants: client.grants,
       userId: client.user_id,
       logoUrl: client.logo_url,
-      author,
+      author: { id: author.id, username: author.username, gender: author.gender },
       scopes: client.scopes === null ? undefined : client.scopes,
       installationsCount: client.installations_count,
     } as OAuth2ClientEntity
