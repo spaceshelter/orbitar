4bcca0d91bdb95a9f3fd61b1f4bd0f37f97f1211
Aivean <aiveeen@gmail.com>
2025-03-04 13:39:39 -0800

    address mobile layout

--- frontend/src/Components/CommentComponent.tsx
+++ frontend/src/Components/CommentComponent.tsx
@@ -249,7 +249,10 @@ export default function CommentComponent(props: CommentProps) {
           </div>
           {props.onAnswer && (
             <div className={styles.control}>
-              <button onClick={handleAnswerSwitch}>{!answerOpen ? 'Ответить' : 'Не отвечать'}</button>
+              <button onClick={handleAnswerSwitch} className={styles.answerButton}>
+                <span className={styles.answerText}>{!answerOpen ? 'Ответить' : 'Не отвечать'}</span>
+                <span className={styles.answerIcon + (!answerOpen ? ' i i-send' : ' i i-close')}></span>
+              </button>
             </div>
           )}
 
