7e1dcede370c16e9e6ae5283941df3cc91e6d1b4
Ivan S <eye.splitter@gmail.com>
2025-03-18 23:18:51 +0200

    add missing poll types

---
+++ frontend/src/API/types/PollEntity.ts
@@ -0,0 +1,19 @@
+export interface PollOptionEntity {
+  text: string
+  votes: number
+}
+
+export interface PollEntity {
+  poll_id: number
+  author_id: number
+  question: string
+  options: PollOptionEntity[]
+  settings: {
+    multiple_choice?: boolean
+    hide_results?: boolean
+  }
+  expires_at: string | null
+  created_at: string
+  total_votes: number
+  user_vote?: number[]
+}
