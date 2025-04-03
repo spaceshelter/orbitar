a2290108a2aec4a6404d584b962a224dc8a9fae1
Ivan S <eye.splitter@gmail.com>
2025-04-04 02:49:00 +0300

    add basic test

---
+++ frontend/cypress.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://orbitar.local',
+    supportFile: 'cypress/support/e2e.ts',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    viewportWidth: 1280,
+    viewportHeight: 720,
+  },
+})
