56875fd77d0b0cf378a2659133e4fa56021cf4e0
Aivean <aiveeen@gmail.com>
2025-04-05 18:04:57 -0700

    disallow poll tag nesting inside other tags

--- backend/src/parser/TheParser.ts
+++ backend/src/parser/TheParser.ts
@@ -86,6 +86,8 @@ export default class TheParser {
       mail: ['a', 'mailbox', 'mail'],
       app: ['a', 'mailbox', 'mail'],
       expand: ['a', 'mailbox', 'mail'],
+      // star means that this tag cannot be inside any other tag
+      poll: '*',
     }
 
     this.parseChildNodesStack = []
@@ -124,7 +126,7 @@ export default class TheParser {
       }
       return false
     }
-    return this.parseChildNodesStack.includes(disallowed)
+    return disallowed === '*' ? this.parseChildNodesStack.length > 0 : this.parseChildNodesStack.includes(disallowed)
   }
 
   private parseChildNodes(doc: ChildNode[]): ParseResult {
