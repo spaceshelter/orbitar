1df9865870315cb3cc022e6f45db083acdaa92d5
Ivan S <eye.splitter@gmail.com>
2025-04-17 23:58:01 +0000

    mediaUploader buttons update

--- frontend/src/Components/MediaUploader.tsx
+++ frontend/src/Components/MediaUploader.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
 
+import Button from '@ui/Button'
 import { toast } from 'react-toastify'
 
 import useFocus from '../API/use/useFocus'
@@ -193,7 +194,9 @@ export default function MediaUploader(props: MediaUploaderProps) {
         body: formData,
       })
         .then((response) => {
-          setUploading(false)
+          setTimeout(() => {
+            setUploading(false)
+          }, 1000)
 
           console.log('UPLOAD RESPONSE', response)
 
@@ -227,7 +230,9 @@ export default function MediaUploader(props: MediaUploaderProps) {
           }
         })
         .catch((error) => {
-          setUploading(false)
+          setTimeout(() => {
+            setUploading(false)
+          }, 1000)
           console.error('UPLOAD FAILED', file.type, error)
           handleError('Произошла ошибка при загрузке 🥺')
         })
@@ -260,9 +265,9 @@ export default function MediaUploader(props: MediaUploaderProps) {
               <div className={styles.choose}>Выбрать</div>
             </label>
           </div>
-          <button disabled={!uploadEnabled || uploading} className={styles.done + ' button'} type='submit'>
+          <Button variant='primary' disabled={!uploadEnabled || uploading} type='submit' loading={uploading}>
             {uploading ? 'Загрузка' : 'Фьють'}
-          </button>
+          </Button>
         </form>
         <div
           className={styles.dropbox + (dragActive ? ' ' + styles.active : '')}
