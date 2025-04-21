e6a1eb73f270a0873007c633aeb317fe413a4a66
Aivean <aiveeen@gmail.com>
2025-04-20 17:31:11 -0700

    change edit profile button style to better match the old one

--- frontend/src/Components/UserProfileBio.tsx
+++ frontend/src/Components/UserProfileBio.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react'
 
+import { useAPI, useAppState } from '@state/AppState'
 import Button from '@ui/Button'
 import { toast } from 'react-toastify'
 
-import { useAPI, useAppState } from '../AppState/AppState'
 import ContentComponent from './ContentComponent'
 import CreateCommentComponent from './CreateCommentComponent'
 
@@ -53,7 +53,7 @@ export default function UserProfileBio(props: UserProfileBioProps) {
       <div className={styles.controls}>
         {props.mine && !editing && (
           <div className={postStyles.control}>
-            <Button variant='ghost' onClick={() => setEditing(true)}>
+            <Button variant='minimal' onClick={() => setEditing(true)}>
               <EditIcon /> {source ? 'Редактировать' : 'Расскажите что-нибудь о себе'}
             </Button>
           </div>
