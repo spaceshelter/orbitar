3ae4274dc439b4883346a625f1139d4917a70dc2
Ivan S <eye.splitter@gmail.com>
2025-04-22 00:18:06 +0300

    try to run inside a docker

--- e2e/cypress.config.ts
+++ e2e/cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://orbitar.local',
+    baseUrl: 'http://localhost:3000',
     supportFile: 'cypress/support/e2e.ts',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     viewportWidth: 1280,
