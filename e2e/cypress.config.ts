0144197a2ca85c8b0a1ff11b5ea9a6581d2bbdec
Ivan S <eye.splitter@gmail.com>
2025-04-24 16:52:25 +0300

    correct url

--- e2e/cypress.config.ts
+++ e2e/cypress.config.ts
@@ -2,14 +2,10 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://test.orbitar.local',
     supportFile: 'cypress/support/e2e.ts',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     viewportWidth: 1280,
     viewportHeight: 720,
-    env: {
-      username: 'testuser',
-      password: 'testpassword',
-    },
   },
 })
