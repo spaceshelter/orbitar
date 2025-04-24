4c7aac80879b4107c4f6da1ed3645440c14467eb
Aivean <aiveeen@gmail.com>
2025-04-23 17:27:32 -0700

    add a tooltip for long poll options

--- frontend/src/Components/Poll/PollComponent.tsx
+++ frontend/src/Components/Poll/PollComponent.tsx
@@ -2,6 +2,7 @@ import React, { useCallback, useEffect, useState } from 'react'
 
 import { PollEntity, ResultVisibility, VoteAccess } from '@api/types/Poll'
 import DateComponent from '@components/DateComponent'
+import { PollOptionTextWithTooltip } from '@components/Poll/PollOptionTextTooltip'
 import { RefreshButton } from '@components/Poll/RefreshButton'
 import { VotersTooltip } from '@components/Poll/VotersList'
 import { useAPI, useAppState } from '@state/AppState'
@@ -170,9 +171,9 @@ export const PollComponent: React.FC<PollProps> = ({ pollId }) => {
                 />
               )}
             </div>
-            <span className={styles.optionText} title={option.text}>
-              {option.text}
-            </span>
+            <div className={styles.optionText}>
+              <PollOptionTextWithTooltip>{option.text}</PollOptionTextWithTooltip>
+            </div>
             <div className={styles.votesContainer}>
               {canShowResults && (
                 <span className={styles.votes}>
