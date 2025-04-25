3dc384b93cff4eb43c7d3ac017d09cfeb3268592
Ivan S <eye.splitter@gmail.com>
2025-04-25 22:55:58 +0300

    fix testusers passwords

--- mysql/test-users.sql
+++ mysql/test-users.sql
@@ -1,16 +1,15 @@
--- Удаляем существующих тестовых пользователей
+USE orbitar_db;
+
 DELETE FROM user_invites WHERE child_id IN (SELECT user_id FROM users WHERE username LIKE 'testuser%');
 DELETE FROM invites WHERE code LIKE 'invite-testuser%';
 DELETE FROM users WHERE username LIKE 'testuser%';
 
--- Создаем тестовых пользователей
 INSERT INTO users (username, password, email, gender, karma, name, registered_at)
 VALUES 
-  ('testuser1', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'test1@example.com', 0, 0, 'Test User 1', CURRENT_TIMESTAMP),
-  ('testuser2', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'test2@example.com', 0, 0, 'Test User 2', CURRENT_TIMESTAMP),
-  ('testuser3', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'test3@example.com', 0, 0, 'Test User 3', CURRENT_TIMESTAMP);
+  ('testuser1', '$2a$10$otySLU3y9XONpFnPqX.OfeNZrCeq4Ld.zC1RCXPCS4YAEGAlYEstO', 'test1@example.com', 0, 0, 'Test User 1', CURRENT_TIMESTAMP),
+  ('testuser2', '$2a$10$K16MyMHXgXY0wtn87bp0VuC31w5sJyjekaeW75hMyinWITbT6ICBO', 'test2@example.com', 0, 0, 'Test User 2', CURRENT_TIMESTAMP),
+  ('testuser3', '$2a$10$aFoEqmuFgApaO8hen3G9I.7YxJrPxkoYr6D0ElT2NlhZRHUB8SWJu', 'test3@example.com', 0, 0, 'Test User 3', CURRENT_TIMESTAMP);
 
--- Создаем инвайты для тестовых пользователей
 INSERT INTO invites (code, issued_by, issued_at, issued_count, left_count)
 SELECT 
   CONCAT('invite-', username),
@@ -21,7 +20,6 @@ SELECT
 FROM users 
 WHERE username LIKE 'testuser%';
 
--- Добавляем записи в user_invites
 INSERT INTO user_invites (parent_id, child_id, invited, invite_id)
 SELECT 
   0,
