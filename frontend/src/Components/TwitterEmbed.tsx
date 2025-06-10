b9d15c13f6c1d2be51f51309d23eb4707f4b1011
Ivan Zaitsev <aiveeen@gmail.com>
2025-06-09 21:27:33 -0700

    Add Twitter embed support

---
+++ frontend/src/Components/TwitterEmbed.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import { Tweet } from 'react-tweet'
+
+import { useTheme } from '../Theme/ThemeProvider'
+
+import 'react-tweet/dist/twitter-theme/theme.css'
+
+interface TwitterEmbedProps {
+  url: string
+}
+
+export default function TwitterEmbed({ url }: TwitterEmbedProps) {
+  const { theme } = useTheme()
+  const match = url.match(/status\/(\d+)/)
+  if (!match) return null
+  const id = match[1]
+
+  return (
+    <div data-theme={theme} className='tweet-embed'>
+      <Tweet id={id} />
+    </div>
+  )
+}
