599468675b77ffc4ed2c8d4efe92488584b2baac
meandrewdev <andrew.progs@gmail.com>
2025-08-24 05:40:59 +0300

    web push fix

--- backend/src/managers/NotificationManager.ts
+++ backend/src/managers/NotificationManager.ts
@@ -36,6 +36,7 @@ export default class NotificationManager {
     notificationsRepository: NotificationsRepository,
     postRepository: PostRepository,
     userCache: UserCache,
+    siteRepository: SiteRepository,
     siteManagerLazy: () => SiteManager /*FIXME: HAX*/,
     webPushRepository: WebPushRepository,
     vapidConfig: VapidConfig,
@@ -46,6 +47,7 @@ export default class NotificationManager {
     this.commentRepository = commentRepository
     this.notificationsRepository = notificationsRepository
     this.postRepository = postRepository
+    this.siteRepository = siteRepository
     this.userCache = userCache
 
     this.webPushRepository = webPushRepository
@@ -193,7 +195,9 @@ export default class NotificationManager {
     this.userCache.deleteUserStatsCache(forUserId)
 
     // send push in background
-    this.sendWebPush(forUserId, notification).then().catch()
+    this.sendWebPush(forUserId, notification)
+      .then()
+      .catch((err) => this.logger.error(err))
   }
 
   async sendAnswerNotify(forUserId: number, byUserId: number, postId: number, commentId?: number) {
