d23fd02d6f8fc556f7593b308771a6eb786e003b
Aivean <aiveeen@gmail.com>
2026-01-04 17:06:50 -0800

    remove yt preview optimization (thumbnail not always available)

--- frontend/src/Components/ContentComponent.tsx
+++ frontend/src/Components/ContentComponent.tsx
@@ -444,9 +444,6 @@ function processYtEmbed(img: HTMLImageElement) {
 
   if (ytUrl && !img.classList.contains('youtube-embed-processed')) {
     img.classList.add('youtube-embed-processed')
-    if (window.screen.width > 1024) {
-      img.src = img.src.replace('0.jpg', 'maxresdefault.jpg')
-    }
     img.addEventListener('click', (e) => {
       e.preventDefault()
       const iframe = document.createElement('iframe')
