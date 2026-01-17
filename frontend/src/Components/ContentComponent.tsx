32512215303ead2f35e984d9e51246378d4e62c5
meandrewdev <andrew.progs@gmail.com>
2026-01-17 04:24:38 +0200

    fix autocut height

--- frontend/src/Components/ContentComponent.tsx
+++ frontend/src/Components/ContentComponent.tsx
@@ -65,7 +65,7 @@ declare global {
   }
 }
 
-export const LARGE_AUTO_CUT = 650
+export const LARGE_AUTO_CUT = 675
 export const SMALL_AUTO_CUT = 100
 
 const iframeToOriginalEl = new WeakMap<HTMLIFrameElement, HTMLElement>()
